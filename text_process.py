4636856a276505a6e3fd9be7b861092f37338d83
Quynh Nguyen <qmn203@nyu.edu>
2020-12-03 22:10:33 -0500

    add list of term frequency lookup

--- text_process.py
+++ text_process.py
@@ -5,8 +5,9 @@ from collections import Counter
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import codecs
+from typing import List
 
-def raw_count(jargon: str, text: str)-> int:
+def term_freq(jargon: str, text: str)-> int:
     """ return number of occurent of a jargon in str content of an article
         subscripts, number, non-alphabetic, stopwords are ignored
     """
@@ -29,7 +30,30 @@ def raw_count(jargon: str, text: str)-> int:
     stop_words = set(stopwords.words('english'))
     tokens = [w for w in tokens if not w in stop_words]
     # bag of words for this document: a dictionary of word: number of counts
-    bow = Counter(tokens)
+    #bow = Counter(tokens) # then return bow[jargon.lower()]
     
-    return bow[jargon.lower()]
+    return tokens.count(jargon.lower()) # only count the word of interest
 
+def terms_freq(jargons_list: List[str], text: str, method: str)-> List[int]:
+    """ for each jargon in jargon lists, return a number of its occurence in str content of text.
+        both jargons and text are converted to lower case before counting
+        methods: 
+            raw: raw count
+            bool: False for raw count = 0, True for >= 1
+            norm: raw count/ processed text length  
+        subscripts, number, non-alphabetic, stopwords can't be used in norm method 
+    """
+    # apply minimal processing for raw count
+    text = codecs.decode(text, 'unicode_escape') # convert \\n to \n in text so tokenizer knows to split 
+    tokens = word_tokenize(text) # split text into words
+    tokens = [w.lower() for w in tokens]  # convert to lower case
+    if method == 'raw': # raw count
+        return [tokens.count(jargon.lower()) for jargon in jargons_list]
+    if method == 'bool':
+        return [tokens.count(jargon.lower()) >= 1 for jargon in jargons_list]
+    tokens = [word for word in tokens if word.isalpha()] # remove tokens that are not alphabetic
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w in stop_words]  # filter out stop words (which is all lower case)
+    if method == 'norm':
+        return [tokens.count(jargon.lower())/len(tokens) for jargon in jargons_list]
+    raise(Exception) # no method exist   
