60fd629c223a8a4294f028e1bf6b065f4c8e2a35
Sinclert Pérez <Sinclert@hotmail.com>
2021-02-01 18:15:29 -0500

    Add new file parsers to __init__

--- src/parsers/__init__.py
+++ src/parsers/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 
-"""
-This module will contain the lower level imports in order to
-simplify their usage from other packages
-"""
+from .base import BaseFileParser
+from .parser_pdf import PDFFileParser
