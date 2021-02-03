153ef39905b42f574419b5a6c3a171c191a2976c
Sinclert Pérez <Sinclert@hotmail.com>
2021-02-03 15:58:43 -0500

    Add new data-input classes to __init__

--- src/data_input/__init__.py
+++ src/data_input/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 
-"""
-This module will contain the lower level imports in order to
-simplify their usage from other packages
-"""
+from .base import BaseDataInput
+from .remote_apis import ArxivAPI
