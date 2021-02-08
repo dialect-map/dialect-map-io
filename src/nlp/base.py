52d2843bab2d6bd3ce66c8bd1f2c76f73ba4c0ed
Sinclert Pérez <Sinclert@hotmail.com>
2021-02-08 15:44:10 -0500

    Define base metrics-engine class

--- src/nlp/base.py
+++ src/nlp/base.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 
-"""
-This module will contain the interface/s referring a set of
-Natural Language Processing classes
-"""
+from abc import ABCMeta
+from abc import abstractmethod
+
+
+class BaseMetricsEngine(metaclass=ABCMeta):
+    """ Interface for the NLP metrics computation """
+
+    @abstractmethod
+    def compute_abs_freq(self, term: str) -> int:
+        """
+        Calculates the absolute frequency of the given term
+        :param term: jargon term to compute the frequency about
+        :return: number of occurrences
+        """
+
+        raise NotImplementedError()
+
+    @abstractmethod
+    def compute_rel_freq(self, term: str) -> float:
+        """
+        Calculates the relative frequency of the given term
+        :param term: jargon term to compute the frequency about
+        :return: relative frequency
+        """
+
+        raise NotImplementedError()
