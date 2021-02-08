9a1930c0a6bfd923e4634dd7e9c6fb621119c37b
Sinclert Pérez <Sinclert@hotmail.com>
2021-02-08 14:44:45 -0500

    Define JSON and PDF input classes

--- src/data_input/file_systems.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-This module will contain classes to deal with file system data files
-"""
---
+++ src/data_input/local_files.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import json
+from typing import Generator
+
+from ..parsers import JSONFileParser
+from ..parsers import PDFFileParser
+
+
+class LocalJSONFile:
+    """ Class containing the contents of a JSON file """
+
+    def __init__(self, file_path: str, file_parser: JSONFileParser):
+        """
+        Initializes the object and parses the JSON contents
+        :param file_path: path to the JSON file
+        :param file_parser: parser for the JSON file
+        """
+
+        self.parser = file_parser
+        self.content = file_parser.extract_text(file_path)
+        self.struct = json.loads(self.content)
+
+    def all_items(self) -> Generator:
+        """
+        Iterates over the top-level JSON array
+        :return: deeper-level object
+        """
+
+        if type(self.struct) != list:
+            raise ValueError("The JSON file must be a top-level array")
+
+        for item in self.struct:
+            yield item
+
+
+class LocalPDFFile:
+    """ Class containing the contents of a PDF file """
+
+    def __init__(self, file_path: str, file_parser: PDFFileParser):
+        """
+        Initializes the object and parses the PDF contents
+        :param file_path: path to the PDF file
+        :param file_parser: parser for the PDF file
+        """
+
+        self.parser = file_parser
+        self.content = file_parser.extract_text(file_path)
+
+    def all_lines(self) -> str:
+        """
+        Iterates over the PDF content text lines
+        :return: text line
+        """
+
+        for line in self.content.splitlines():
+            yield line
