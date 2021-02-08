dcca91545dd603b35fc0a4abff6491635aee615e
Sinclert Pérez <Sinclert@hotmail.com>
2021-02-08 16:59:57 -0500

    Define missing TXT file input class

--- src/data_input/__init__.py
+++ src/data_input/__init__.py
@@ -4,5 +4,6 @@ from .base import BaseRemoteInput
 
 from .local_files import LocalJSONFile
 from .local_files import LocalPDFFile
+from .local_files import LocalTextFile
 
 from .remote_apis import ArxivAPI
