dfb6f57c29f62409e21162d0eb51f79b8c6a572d
Sinclert Pérez <Sinclert@hotmail.com>
2021-02-11 11:18:10 -0500

    Simplify data-output API class

--- src/data_output/__init__.py
+++ src/data_output/__init__.py
@@ -5,4 +5,4 @@ from .local_files import JSONFileWriter
 from .local_files import TextFileWriter
 
 from .remote_apis import BaseAPIOutput
-from .remote_apis import PrivateAPIOutput
+from .remote_apis import DialectMapAPI
