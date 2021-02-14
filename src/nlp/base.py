48659932fc7ac6571a0883db9b7f68d952c17129
Quynh Nguyen <qmn203@nyu.edu>
2021-02-14 15:51:18 -0500

    refactor: implement FuzzyMetricsEngine

--- src/nlp/base.py
+++ src/nlp/base.py
@@ -2,27 +2,28 @@
 
 from abc import ABCMeta
 from abc import abstractmethod
+from typing import List
 
 
 class BaseMetricsEngine(metaclass=ABCMeta):
     """ Interface for the NLP metrics computation """
 
     @abstractmethod
-    def compute_abs_freq(self, term: str) -> int:
+    def compute_abs_freq(self, terms: List[str], text: str) -> List[dict]:
         """
-        Calculates the absolute frequency of the given term
-        :param term: jargon term to compute the frequency about
-        :return: number of occurrences
+        Calculates the absolute frequency (raw count of number of occurence) of the given term
+        :param terms: list of jargon term to compute the frequency about
+        :param text: text of the document
+        :return: a list of dictionary , each corresponds to a jargon { 'jargon': str, 'frequency': int }
         """
-
-        raise NotImplementedError()
+        pass
 
     @abstractmethod
-    def compute_rel_freq(self, term: str) -> float:
+    def compute_rel_freq(self, terms: List[str], text: str) -> List[dict]:
         """
-        Calculates the relative frequency of the given term
-        :param term: jargon term to compute the frequency about
-        :return: relative frequency
+        Calculates the relative frequency (raw count / document length) of the given term
+        :param terms: list of jargon term to compute the frequency about
+        :param text: text of the document
+        :return: a list of dictionary , each corresponds to a jargon { 'jargon': str, 'frequency': float }
         """
-
-        raise NotImplementedError()
+        pass
