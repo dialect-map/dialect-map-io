0c9c588683d940b23c0d89b097dbc5a4e3875ff3
Sinclert Pérez <Sinclert@hotmail.com>
2021-03-17 12:10:43 -0400

    Remove file parsers old file

--- src/dialect_map_io/parsers/__init__.py
+++ src/dialect_map_io/parsers/__init__.py
@@ -6,11 +6,6 @@ from .parsers_data import JSONDataParser
 from .parsers_feed import BaseFeedParser
 from .parsers_feed import ArxivFeedParser
 
-from .parsers_file import BaseFileParser
-from .parsers_file import JSONFileParser
-from .parsers_file import PDFFileParser
-from .parsers_file import TextFileParser
-
 from .parsers_text import BaseTextParser
 from .parsers_text import PDFTextParser
 from .parsers_text import TXTTextParser
