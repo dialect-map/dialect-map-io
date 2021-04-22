4befe06782711c2297186972350eb5b4f6ed92d9
Sinclert Pérez <Sinclert@hotmail.com>
2021-04-22 09:53:05 -0400

    Create new RestInputAPI class

--- src/dialect_map_io/data_input/remote_apis.py
+++ src/dialect_map_io/data_input/remote_apis.py
@@ -106,3 +106,74 @@ class ArxivInputAPI(BaseInputAPI):
         """
 
         return self.request_data("/query", {"id_list": paper_id})
+
+
+class RestInputAPI(BaseInputAPI):
+    """Class for requesting data from REST APIs"""
+
+    def __init__(self, base_url: str):
+        """
+        Initializes the remote API input reference
+        :param base_url: remote API complete URL
+        """
+
+        self.base_url = base_url.rstrip("/")
+
+    @staticmethod
+    def _check_response(response: Response):
+        """
+        Checks the status of the response for HTTP 4XX and 5XX codes
+        :param response: raw API response
+        """
+
+        try:
+            response.raise_for_status()
+        except requests.HTTPError:
+            logger.error(f"The API response does not have a valid HTTP code")
+            logger.error(f"Error: {response.text}")
+            raise ConnectionError(response.text)
+
+    @staticmethod
+    def _decode_response(response: Response) -> dict:
+        """
+        Decodes the response assuming a serialized JSON
+        :param response: raw API response
+        :return: dictionary API response
+        """
+
+        try:
+            json = response.json()
+        except ValueError:
+            logger.error(f"The API response does not contains a valid JSON")
+            logger.error(f"Response: {response.text}")
+            raise
+        else:
+            return json
+
+    def _perform_request(self, api_path: str, api_args: dict) -> Response:
+        """
+        Performs a HTTP GET request to the given API path
+        :param api_path: API path to send the request
+        :param api_args: API args to tune the request
+        :return: API response
+        """
+
+        response = requests.get(
+            url=f"{self.base_url}{api_path}",
+            params=api_args,
+        )
+
+        self._check_response(response)
+        return response
+
+    def request_data(self, api_path: str, api_args: dict) -> dict:
+        """
+        Requests JSON data from a public API
+        :param api_path: API path to send the request
+        :param api_args: API args to tune the request
+        :return: JSON-encoded response
+        """
+
+        resp = self._perform_request(api_path, api_args)
+        resp = self._decode_response(resp)
+        return resp
