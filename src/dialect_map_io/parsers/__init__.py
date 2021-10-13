1ba08b18ca1a2f112b1459b47ded80aa207ad105
Sinclert Pérez <Sinclert@hotmail.com>
2021-10-13 11:20:00 -0400

    Remove Feed metadata parser from old folder

--- src/dialect_map_io/parsers/__init__.py
+++ src/dialect_map_io/parsers/__init__.py
@@ -6,9 +6,6 @@ from .metadata import FeedMetadataParser
 from .parsers_data import BaseDataParser
 from .parsers_data import JSONDataParser
 
-from .parsers_feed import BaseFeedParser
-from .parsers_feed import ArxivFeedParser
-
 from .parsers_text import BaseTextParser
 from .parsers_text import PDFTextParser
 from .parsers_text import TXTTextParser
