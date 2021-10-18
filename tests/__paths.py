1318289506c7de0b464aff6947e46abc6665a5ce
Sinclert Pérez <Sinclert@hotmail.com>
2021-10-18 15:27:40 -0400

    Adapt tests __paths.py module

--- tests/__paths.py
+++ tests/__paths.py
@@ -6,13 +6,7 @@ from pathlib import Path
 PROJECT_PATH = Path(__file__).parent
 
 DATA_FOLDER = PROJECT_PATH.joinpath(".data")
-META_FOLDER = PROJECT_PATH.joinpath(".metadata")
 
-# Samples of what is considered 'data'
-DATA_JSON_FOLDER = DATA_FOLDER.joinpath("json")
-DATA_PDF_FOLDER = DATA_FOLDER.joinpath("pdf")
-DATA_TXT_FOLDER = DATA_FOLDER.joinpath("txt")
-
-# Samples of what is considered 'metadata'
-META_FEED_FOLDER = META_FOLDER.joinpath("feed")
-META_JSON_FOLDER = META_FOLDER.joinpath("json")
+JSON_FOLDER = DATA_FOLDER.joinpath("json")
+PDF_FOLDER = DATA_FOLDER.joinpath("pdf")
+TXT_FOLDER = DATA_FOLDER.joinpath("txt")
