9a8eb6fb5a5ea045a9a92826f445dc8f4ade00d5
Sinclert Pérez <sinclert@hotmail.com>
2022-07-25 15:14:32 +0200

    Define JSONPlainDecoder class

--- src/dialect_map_io/encoding/decoders/__init__.py
+++ src/dialect_map_io/encoding/decoders/__init__.py
@@ -5,3 +5,5 @@ from .base import BaseBinaryDecoder
 from .base import BasePlainDecoder
 
 from .decoder_binary import PDFBinaryDecoder
+
+from .decoder_plain import JSONPlainDecoder
---
+++ src/dialect_map_io/encoding/decoders/decoder_plain.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from json import JSONDecoder
+from json import JSONDecodeError
+from typing import AnyStr
+
+from .base import BasePlainDecoder
+
+
+logger = logging.getLogger()
+
+
+class JSONPlainDecoder(BasePlainDecoder):
+    """JSON contents decoder class"""
+
+    def __init__(self, **kwargs):
+        """
+        Initializes the JSON content decoder
+        :param kwargs: keyword arguments for the default decoder
+        """
+
+        self._decoder = JSONDecoder(**kwargs)
+
+    def decode(self, data: AnyStr) -> object:
+        """
+        Decodes a string/bytes blob into a Python object
+        :param data: string/bytes blob
+        :return: Python object
+        """
+
+        string = self._decode(data)
+
+        try:
+            obj = self._decoder.decode(string)
+        except JSONDecodeError:
+            logger.error(f"The provided data does not contains a valid JSON")
+            logger.error(f"Data: {string}")
+            raise
+
+        return obj
